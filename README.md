75da5ec2d53e1b28c953dc2167d3c68f92e0d806
Kosuke Ito <keree.it.owl.uk@gmail.com>
2023-12-03 02:05:25 +0900

    fix

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 youtubeの字幕を作るために使うプログラムのリポジトリです。
+
+
+## 使い方
+python-dotenvをインストール
+```
+pip install python-dotenv
+```
--- video_to_audio_converter.py
+++ video_to_audio_converter.py
@@ -1,4 +1,13 @@
 from moviepy.editor import VideoFileClip
+from dotenv import load_dotenv
+import os
+
+# 環境変数の読み込み
+load_dotenv()
+
+# 環境変数からパスを取得
+OUTPUT_AUDIO_PATH = os.getenv("OUTPUT_AUDIO_PATH")
+INPUT_MOVIE_PATH = os.getenv("INPUT_MOVIE_PATH")
 
 def extract_audio(video_file_path, audio_file_path):
   # ビデオファイルを読み込む
@@ -7,9 +16,4 @@ def extract_audio(video_file_path, audio_file_path):
   # ビデオから音声を抽出し、ファイルとして保存する
   video.audio.write_audiofile(audio_file_path)
 
-# 使用例
-
-video_file_path = ''  # ここに動画ファイルのパスを入力
-audio_file_path = ''  # ここに抽出した音声を保存するファイル名のパス名
-
-extract_audio(video_file_path, audio_file_path)
+extract_audio(INPUT_MOVIE_PATH, OUTPUT_AUDIO_PATH)
